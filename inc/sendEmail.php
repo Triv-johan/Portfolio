6af01e490a95a651d03dd811a2226bb96ae93ff0
Triv-johan <guyon.johan@laposte.net>
2020-12-14 13:45:44 +0100

    fgfgfgf

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -13,15 +13,15 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Merci d'entrer votre nom.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "S'il vous plait veuillez saisir une adresse correct.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Votre message doit contenir plus de 15 caractère .";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
@@ -32,7 +32,7 @@ if($_POST) {
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+   $message .= "<br /> ----- <br /> Cette email a ete envoyer depuis le formulaire. <br />";
 
    // Set From: header
    $from =  $name . " <" . $email . ">";
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Un probleme est survenue, veuillez réesayez."; }
 		
 	} # end if - no validation error
 
